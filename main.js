d32bcf93d5211e017e7735b4a087859bdeffd06d
Alonso Alarcon <155941412+alxxjandro@users.noreply.github.com>
2024-02-05 11:41:48 -0600

    Edited the js file, as well as the cs and html files. Now the grid is generated as it should. It can be changed in the code.

--- main.js
+++ main.js
@@ -1,30 +1,19 @@
+const bodyContainer = document.querySelector('.gridContainer'); 
 
-const gridContainer = document.querySelector('#gridContainer');
 
-//You give que amount of slots per-column
-function createColumn(numberOfSlots){
+function createGrid(numberOfSlots) {
+    for (let i = 0; i < numberOfSlots; i++) {
+        let column = document.createElement('div');
+        column.classList.add('columns');
+        bodyContainer.append(column);
 
-    //gets saved on a div of each own 
-    const rowContainer = document.createElement('div');
-    gridContainer.append(rowContainer);
-
-    //generate each cube-slot
-    for (let i = 0; i < numberOfSlots; i++){
-        const cube = document.createElement('div');
-        cube.classList.add('gridCube');
-        rowContainer.append(cube);
+        for (let j = 0; j < numberOfSlots; j++) {
+            let row = document.createElement('div');
+            row.classList.add('row');
+            column.append(row);
+        }
     }
 }
 
-//generate a row using the columns function multiple times
-function createGrid(numberOfSlots){
-    for (let i = 0; i < numberOfSlots; i++){
-        createColumn(numberOfSlots);
-    }
-}
 
 createGrid(16);
-
-
-
-
