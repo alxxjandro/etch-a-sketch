22a2c69f18384f9d2eae97a51aaf9b46691ab205
Alonso Alarcon <155941412+alxxjandro@users.noreply.github.com>
2024-02-05 12:50:53 -0600

    Everything works as intended. Going to start making desing changes to make it look better.

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 const bodyContainer = document.querySelector('.gridContainer'); 
 let color = 'black'
 
+//defining the buttons
+const blackInkBtn = document.querySelector('#blackInk');
+const randomColorBtn = document.querySelector('#randomColor');
+const resizeBtn = document.querySelector('#resizeBtn')
+const resetBtn = document.querySelector('#resetBtn');
+
+
 
 function createGrid(numberOfSlots) {
+
+    bodyContainer.innerHTML = ''; //erase the previous grid before drawing one
+
     for (let i = 0; i < numberOfSlots; i++) {
         let column = document.createElement('div');
         column.classList.add('columns');
@@ -13,6 +23,7 @@ function createGrid(numberOfSlots) {
             row.classList.add('row');
             column.append(row);
 
+            //pass over which row is the mouse hovering into
             row.addEventListener('mouseover', () =>{
                 changeColor(row,color)
             })
@@ -20,11 +31,44 @@ function createGrid(numberOfSlots) {
     }
 }
 
+
+//this part makes the buttons work
 function changeColor(slot,color){
     slot.style.backgroundColor = color;
 }
 
+function getRandomColor(){
+    return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+randomColorBtn.addEventListener('click', () =>{
+    color = getRandomColor();
+})
+
+blackInkBtn.addEventListener('click',() =>{
+    color = 'black'
+})
+
+resizeBtn.addEventListener('click',() => {
+    let scale;
+    do{
+        scale = parseInt(prompt('Enter a number between 1 and 99 to resize the grid!'))
+    } while (scale < 1 || scale > 99);
+    createGrid(scale);
+    
+})
 
 
-createGrid(10);
+//reset
+resetBtn.addEventListener('click', () =>{
+    const gridCubes = document.querySelectorAll('.row');
+    gridCubes.forEach(function(cube,index){
+        cube.style.backgroundColor = 'white'
+    })
+})
+
+createGrid(16);
+
+
+
 
