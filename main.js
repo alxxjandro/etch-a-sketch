4c14602aca37e72f7c7c764d1a4a7497c4761ff8
Alonso Alarcon <155941412+alxxjandro@users.noreply.github.com>
2024-02-05 11:51:55 -0600

    Modified the createGrid() in order to make the black ink to work.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const bodyContainer = document.querySelector('.gridContainer'); 
+let color = 'black'
 
 
 function createGrid(numberOfSlots) {
@@ -11,9 +12,19 @@ function createGrid(numberOfSlots) {
             let row = document.createElement('div');
             row.classList.add('row');
             column.append(row);
+
+            row.addEventListener('mouseover', () =>{
+                changeColor(row,color)
+            })
         }
     }
 }
 
+function changeColor(slot,color){
+    slot.style.backgroundColor = color;
+}
 
-createGrid(16);
+
+
+createGrid(10);
+
